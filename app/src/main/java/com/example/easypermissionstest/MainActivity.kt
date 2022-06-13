4f4558552ccaa7ceb9b393065d6d5212822b3481
mraheeldev <m.raheeldev@gmail.com>
2022-06-13 16:11:29 +0500

    Initial commit

---
+++ app/src/main/java/com/example/easypermissionstest/MainActivity.kt
@@ -0,0 +1,80 @@
+package com.example.easypermissionstest
+
+import android.Manifest
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+
+
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
+
+    lateinit var button: Button
+
+    private val permissionsPerms = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        arrayOf(
+            Manifest.permission.ACCESS_MEDIA_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button_open_camera)
+
+        button.setOnClickListener {
+            openCamera()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    private fun openCamera() {
+
+        if (EasyPermissions.hasPermissions(this, *permissionsPerms)) {
+            Log.d("myPermission", "hasPermissions allow")
+        } else {
+            EasyPermissions.requestPermissions(
+                this, "We need permissions because this and that",
+                123, *permissionsPerms
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        Log.d("myPermission", "allow")
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        Log.d("myPermission", "not allow")
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AppSettingsDialog.DEFAULT_SETTINGS_REQ_CODE) {
+            Log.d("myPermission", "request to Camera")
+        }
+    }
+}
